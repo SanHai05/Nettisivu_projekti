0050105baa79125cc4598f8709d4c4d2b259014a
SanHai05 <sannihaimilahti05@gmail.com>
2026-03-01 13:11:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const lightbox = GLightbox({
     keyboardNavigation: true
 });
 
+document.getElementById('open-eye-gallery')?.addEventListener('click', function(e) {
+    e.preventDefault();
+    // Etsitään se silmäkuva, joka on oikeasti osa galleriaa (mobiiliversio)
+    // ja simuloidaan klikkaus siihen.
+    const actualGalleryItem = document.querySelector('.d-lg-none .glightbox');
+    if (actualGalleryItem) {
+        actualGalleryItem.click();
+    }
+});
+
+
